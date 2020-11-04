761825e4cc852d63ed6b971e0e360b621bc9a554
Christian <alvescastro00cca13@gmail.com>
2020-11-04 12:15:46 -0300

    @Req testing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get} from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res, Request, Response } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
+  //
   // @Get()
   // getHello(): string {
   //   return this.appService.getHello();
   // }
 
+
+  //Usando os métodos de tratamento de respostas nativos
   @Get('users')
-  getBye(): string {
-    return this.appService.getBye();
+  getBye(@Response() response): JSON {
+    return response.status(200).json({
+      "Nome": "Chris",
+      "Idade": 20
+    })
+
   }
+
+
+  // @Get('users')
+  // getBye(@Req() request: Request): string{
+  //   return 'Testando o Request'
+  // }
+
 }
