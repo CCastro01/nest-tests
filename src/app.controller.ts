ddefe4f1b8ef8047f778b060c35ef1a7972ed17c
Christian <alvescastro00cca13@gmail.com>
2020-11-04 12:51:05 -0300

    Route param test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, HttpCode, Req, Res, Request, Response } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  HttpCode, 
+  Req, Res, 
+  Request, 
+  Response, 
+  Post, 
+  Redirect,
+  Param
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('v1')
@@ -13,19 +23,26 @@ export class AppController {
 
 
   //Usando os métodos de tratamento de respostas nativos
-  @Get('users')
-  getBye(@Response() response): JSON {
-    return response.status(200).json({
-      "Nome": "Chris",
-      "Idade": 20
-    })
+  // @Get('users')
+  // @HttpCode(200)
+  // getBye(@Response() response): JSON {
+  //   return response.json({
+  //     "Nome": "Chris",
+  //     "Idade": 20
+  //   })
 
-  }
-
+  // }
 
   // @Get('users')
   // getBye(@Req() request: Request): string{
   //   return 'Testando o Request'
   // }
+
+  // ROUTE PARAMS
+  @Get(':id')
+  findOne(@Param('id') id): string{
+    return `This action returns a ${id} user`
+  }
+
 
 }
