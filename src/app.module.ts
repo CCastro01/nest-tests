bfab0d82d5d2324a2b38291a0b904b1015ab8971
Christian <alvescastro00cca13@gmail.com>
2020-11-05 12:43:12 -0300

    Adding Service and Module to Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [UsersController],
-  providers: [],
+  providers: [UsersService],
 })
 export class UserModule {}
