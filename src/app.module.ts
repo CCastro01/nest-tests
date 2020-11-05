3ccc4a0674ad4eb8cf4e4eaa813ca5e97a50e6e7
Christian <alvescastro00cca13@gmail.com>
2020-11-05 12:47:25 -0300

    Refactoring app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [],
+  providers: [],
 })
 export class UserModule {}
