377c8d06a7a38512fb4072123bffe01d152cce52
Christian <alvescastro00cca13@gmail.com>
2020-11-06 13:01:32 -0300

    CreateUserDto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import {CreateUserDto} from './dto/create-user.dto'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    users: CreateUserDto[] = [
+        {id: 1, name: "user1", email: "user1@dellead.com", password: 'senha1'},
+        {id: 2, name: "user2", email: "user2@dellead.com", password: 'senha2'},
+        {id: 3, name: "user3", email: "user3@dellead.com", password: 'senha3'}
+    ]
+}
